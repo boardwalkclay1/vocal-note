d768f37fbbb9b4f2fcf005207fdb77dc6b08c959
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-05 23:00:42 -0400

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "vocal-note-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "/icons/favicon-32.png",
+  "/icons/favicon-16.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((k) => k !== CACHE_NAME)
+          .map((k) => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
